86958be6e6f08e095e1d17a93f7a3d7961241a94
shagunavid <shagun@avidsecure.io>
2018-12-14 17:17:01 +0530

    shagun

--- demo_file.tf
+++ demo_file.tf
@@ -50,6 +50,57 @@ resource "aws_lb" "test" {
 #testing
 
 
+
+resource "aws_cloudtrail" "example" {
+
+  is_multi_region_trail = true
+
+  cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+  event_selector {
+    read_write_type = "All"
+    include_management_events = true
+
+    data_resource {
+      type   = "AWS::Lambda::Function"
+      values = ["arn:aws:lambda"]
+    }
+  }
+}
+#Ensure a log metric filter and alarm exist for Management Console sign-in without MFA
+resource "aws_cloudwatch_log_metric_filter" "MFAUsed" {
+  name           = "console-without-mfa"
+  pattern        = "{$.eventName = \"ConsoleLogin\"}"
+  log_group_name = "someLogGroup"
+
+  metric_transformation {
+    name      = "ConsoleWithoutMFACount"
+    namespace = "someNamespace"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "console_without_mfa" {
+  alarm_name          = "console-without-mfa-us-west-2"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ConsoleWithoutMFACount"
+  namespace           = "someNamespace"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "Use of the console by an account without MFA has been detected"
+  alarm_actions       = ["someTopic"]
+}
+
+resource "aws_sns_topic" "security_alerts" {
+  name  = "someTopic"
+  arn   = "someTopic"
+
+}
+
+
+
+
 #Ensure SNS topics do not allow global send or subscribe
 
 resource "aws_sns_topic_policy" "sns_policy" {
